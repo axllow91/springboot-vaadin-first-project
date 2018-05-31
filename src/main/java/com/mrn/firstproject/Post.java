8da5502e94e555c7859c79bbb4470a326934fd1e
Dobre Marian <dobremi91@gmail.com>
2018-05-31 14:12:35 +0300

    First Vaadin, Spring boot project. In this simple project i parse some JSON objects.

---
+++ src/main/java/com/mrn/firstproject/Post.java
@@ -0,0 +1,27 @@
+package com.mrn.firstproject;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true) // ignore anything that doesn't map to what we have
+public class Post {
+    private int userId;
+    private int id;
+    private String title;
+    private String  body;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
---
+++ src/main/java/com/mrn/firstproject/Service.java
@@ -0,0 +1,34 @@
+package com.mrn.firstproject;
+
+import org.springframework.web.client.RestTemplate;
+
+@org.springframework.stereotype.Service
+public class Service {
+
+    private String url = "https://jsonplaceholder.typicode.com/posts/1";
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    // Here we are mapping our Post model to the JSON object from remoteplace
+    Post post = restTemplate.getForObject(url,Post.class);
+
+    int showUserid() {
+        return post.getUserId();
+    }
+
+    int showId() {
+        return post.getId();
+    }
+
+    String showTitle() {
+        return post.getTitle();
+    }
+
+    String showBody() {
+        return post.getBody();
+    }
+
+    public String showPost() {
+        return post.getBody();
+    }
+}
